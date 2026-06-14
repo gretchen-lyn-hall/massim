4d3829965ed818efe7af42bcd2552ab09ee30a7d
theinfinitegirl <gretchenlynhall@gmail.com>
2026-06-14 13:36:50 -0700

    Prepping for GH workflow

---
+++ README.md
@@ -0,0 +1,28 @@
+# MASSIM - an Ecological simulation framework for synthesizing FT-ICR data.
+
+## Introduction
+
+
+## Installation
+
+MASSIM can be installed using the `pip` package manager with:
+```
+>>> pip install massim
+```
+
+However, note that MASSIM installation requires that C++ extension modules be 
+built, which requires 
+
+
+
+### Windows:
+Python - install, add env vars
+
+
+Downlod VS Installer
+Choose Desktop Development with C++
+Also, in right pane, select:
+C++ Clang Tools for Windows
+C++/CLI Tools
+Windows 11 SDK
+MSVC Build 
--- src/massim/__init__.py
+++ src/massim/__init__.py
@@ -1,15 +1,27 @@
+"""
+MASSIM - Ecological simulation framework for synthesizing FT-ICR data.
 
-try:
-    # There seems to be a conflict between graph_tool and pybind11; at least
-    # with the DLLs created using non-apple Clang. If Massim's libraries are
-    # loaded first, graph-tool will core dump in pthread_create. There is no
-    # problem if graph-tool is loaded first.
-    #import graph_tool as gt
-    pass
-except ImportError:
-    pass
 
-__all__ = ['distribtions',
+"""
+__author__ = "Gretchen Hall (gretchenhall@arizona.edu)"
+from importlib.metadata import version
+__version__ = version("massim")
+
+
+from . import distributions
+from . import experiment
+from . import gradient_response
+from . import species_config
+from . import sampling
+from . import profile
+from . import noise
+from . import filters
+from . import mass_distribution
+from . import stats
+from . import transformations
+from . import data_analysis
+
+__all__ = ['distributions',
            'experiment',
            'gradient_response',
            'species_config',
@@ -18,5 +30,31 @@ __all__ = ['distribtions',
            'noise',
            'filters',
            'mass_distribution',
+           'transformations',
+           'stats',
+           'data_analysis',
            ]
-           
+
+
+# from .distributions import *
+# from .gradient_response import GradientResponse, BetaResponse
+# from .mass_distribution import RandomMasses, MassListPicker
+# from .species_config import (
+#     SpeciesGroupConfig,
+#     GenSpeciesStage,
+#     GenCoreStage,
+#     quick_compas)
+# from .sampling import *
+# from .experiment import (Experiment,
+#                          Message,
+#                          OutputFilter,
+#                          ParameterSweep,
+#                          LinSpace,
+#                          ArraySweep,
+#                          RandomSweep,
+#                          ReplicateStage,
+#                          DebugStage,
+#                          )
+# from .transformations import (TransformStage, make_transform_list)
+# from .stats import *
+
