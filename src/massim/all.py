624782f33f540849fae981e57565bcb5da71fa5c
theinfinitegirl <gretchenlynhall@gmail.com>
2026-06-15 12:13:58 -0700

    Forgot to add 'all.py'; used importlib.resources for data files

---
+++ src/massim/all.py
@@ -0,0 +1,18 @@
+"""
+Helper module to import all MASSIM symbols into a flat namespace.
+Usage:
+>>> import massim.all as massim
+"""
+
+from  .distributions import *
+from  .experiment import *
+from  .gradient_response import *
+from  .species_config import *
+from  .sampling import *
+from  .profile import *
+from  .noise import *
+from  .filters import *
+from  .mass_distribution import *
+from  .stats import *
+from  .transformations import *
+from  .data_analysis import *
